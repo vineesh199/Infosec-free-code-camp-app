f28186e45d7959a202b541665be3200507133a35
vineesh199 <156292027+vineesh199@users.noreply.github.com>
2024-01-17 11:53:27 +0530

    Use helmet framegaurd to deny X-frame info

--- myApp.js
+++ myApp.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const helmet =require('helmet');
 const app = express();
+app.use(helmet.hidePoweredBy({setTo:'PHP 4.2.0'}));
 
 
-
-
-app.use(helmet-hidePoweredBy());
-
-
-
+app.use(helmet.framegaurd({action:'deny'}));
 
 
 
